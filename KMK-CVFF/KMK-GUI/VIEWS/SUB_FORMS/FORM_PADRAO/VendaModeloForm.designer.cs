b617fb547ff66809037fbded9251d4e1223e3267
Ademir Aparecido dos Santos Oliveira <ademirap.santos@outlook.com>
2017-02-09 12:21:15 -0200

    V 0.0.0.2

--- KMK-CVFF/KMK-GUI/VIEWS/SUB_FORMS/FORM_PADRAO/VendaModeloForm.designer.cs
+++ KMK-CVFF/KMK-GUI/VIEWS/SUB_FORMS/FORM_PADRAO/VendaModeloForm.designer.cs
@@ -34,6 +34,7 @@
             this.btnVDMDFRMConcluir = new System.Windows.Forms.Button();
             this.btnVDMDFRMExcluir = new System.Windows.Forms.Button();
             this.lstVDMDFRMProdutos = new System.Windows.Forms.ListBox();
+            this.txtVDMDFRMProdutos = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,9 +49,9 @@
             this.groupBox1.Controls.Add(this.lstVDMDFRMProdutos);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(10, 115);
+            this.groupBox1.Location = new System.Drawing.Point(10, 52);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(533, 354);
+            this.groupBox1.Size = new System.Drawing.Size(810, 457);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Lista de Pedidos";
@@ -62,9 +63,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.Color.Transparent;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel2.Location = new System.Drawing.Point(436, 119);
+            this.panel2.Location = new System.Drawing.Point(712, 128);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(88, 184);
+            this.panel2.Size = new System.Drawing.Size(89, 270);
             this.panel2.TabIndex = 14;
             // 
             // btnVDMDFRMCancelar
@@ -75,9 +76,9 @@
             this.btnVDMDFRMCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
             this.btnVDMDFRMCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVDMDFRMCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVDMDFRMCancelar.Location = new System.Drawing.Point(436, 74);
+            this.btnVDMDFRMCancelar.Location = new System.Drawing.Point(712, 74);
             this.btnVDMDFRMCancelar.Name = "btnVDMDFRMCancelar";
-            this.btnVDMDFRMCancelar.Size = new System.Drawing.Size(88, 39);
+            this.btnVDMDFRMCancelar.Size = new System.Drawing.Size(89, 48);
             this.btnVDMDFRMCancelar.TabIndex = 13;
             this.btnVDMDFRMCancelar.Text = "Cancelar";
             this.btnVDMDFRMCancelar.UseVisualStyleBackColor = true;
@@ -91,9 +92,9 @@
             this.btnVDMDFRMConcluir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
             this.btnVDMDFRMConcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVDMDFRMConcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVDMDFRMConcluir.Location = new System.Drawing.Point(436, 309);
+            this.btnVDMDFRMConcluir.Location = new System.Drawing.Point(712, 404);
             this.btnVDMDFRMConcluir.Name = "btnVDMDFRMConcluir";
-            this.btnVDMDFRMConcluir.Size = new System.Drawing.Size(88, 39);
+            this.btnVDMDFRMConcluir.Size = new System.Drawing.Size(89, 47);
             this.btnVDMDFRMConcluir.TabIndex = 12;
             this.btnVDMDFRMConcluir.Text = "Concluir";
             this.btnVDMDFRMConcluir.UseVisualStyleBackColor = true;
@@ -107,9 +108,9 @@
             this.btnVDMDFRMExcluir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
             this.btnVDMDFRMExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVDMDFRMExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVDMDFRMExcluir.Location = new System.Drawing.Point(436, 29);
+            this.btnVDMDFRMExcluir.Location = new System.Drawing.Point(712, 29);
             this.btnVDMDFRMExcluir.Name = "btnVDMDFRMExcluir";
-            this.btnVDMDFRMExcluir.Size = new System.Drawing.Size(88, 39);
+            this.btnVDMDFRMExcluir.Size = new System.Drawing.Size(89, 39);
             this.btnVDMDFRMExcluir.TabIndex = 7;
             this.btnVDMDFRMExcluir.Text = "Excluir";
             this.btnVDMDFRMExcluir.UseVisualStyleBackColor = true;
@@ -124,15 +125,25 @@
             this.lstVDMDFRMProdutos.ItemHeight = 15;
             this.lstVDMDFRMProdutos.Location = new System.Drawing.Point(6, 29);
             this.lstVDMDFRMProdutos.Name = "lstVDMDFRMProdutos";
-            this.lstVDMDFRMProdutos.Size = new System.Drawing.Size(423, 319);
+            this.lstVDMDFRMProdutos.Size = new System.Drawing.Size(700, 424);
             this.lstVDMDFRMProdutos.TabIndex = 0;
+            // 
+            // txtVDMDFRMProdutos
+            // 
+            this.txtVDMDFRMProdutos.BackColor = System.Drawing.SystemColors.Info;
+            this.txtVDMDFRMProdutos.Enabled = false;
+            this.txtVDMDFRMProdutos.Location = new System.Drawing.Point(722, 12);
+            this.txtVDMDFRMProdutos.Name = "txtVDMDFRMProdutos";
+            this.txtVDMDFRMProdutos.Size = new System.Drawing.Size(98, 20);
+            this.txtVDMDFRMProdutos.TabIndex = 4;
             // 
             // VendaModeloForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(555, 480);
+            this.ClientSize = new System.Drawing.Size(832, 520);
+            this.Controls.Add(this.txtVDMDFRMProdutos);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "VendaModeloForm";
@@ -140,6 +151,7 @@
             this.Text = "VendaModeloForm";
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -151,6 +163,6 @@
         private System.Windows.Forms.Button btnVDMDFRMExcluir;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnVDMDFRMCancelar;
-
+        private System.Windows.Forms.TextBox txtVDMDFRMProdutos;
     }
 }
---
+++ KMK-CVFF/KMK-MODELO/ENUM/Estado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMK_MODELO.ENUM
+{
+    public enum Estado
+    {
+        Acre = 1,
+        Alagoas = 2,
+        Amapá = 3,
+        Amazonas = 4,
+        Bahia =5,
+        Ceará = 6,
+        Distrito_Federal = 7,
+        Espírito_Santo = 8,
+        Goiás = 9,
+        Maranhão = 10,
+        Mato_Grosso = 11,
+        Mato_Grosso_do_Sul = 12,
+        Minas_Gerais = 13,
+        Pará = 14,
+        Paraíba = 15,
+        Paraná = 16,
+        Pernambuco = 17,
+        Piauí = 18,
+        Rio_de_Janeiro = 19,
+        Rio_Grande_do_Norte = 20,
+        Rio_Grande_do_Sul = 21,
+        Rondônia = 22,
+        Roraima = 23,
+        Santa_Catarina = 24,
+        São_Paulo = 25,
+        Sergipe = 26,
+        Tocantins = 27
+    }
+}
